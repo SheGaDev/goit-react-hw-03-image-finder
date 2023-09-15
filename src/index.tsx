6ae8ba19950a080e448591e3521be7f623ce20dc
SheGaDev <61838855+SheGaDev@users.noreply.github.com>
2023-09-15 21:29:52 +0300

    Remove declare env.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,3 @@
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      API_KEY_PIXABAY: string;
-    }
-  }
-}
 import ReactDOM from 'react-dom/client';
 import './global.css';
 import App from './components/app/App';
